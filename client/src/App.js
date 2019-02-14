11f2820f165bdcf39c6f481e05942912de5eadbc
Alyssa Williams <awilliams@Wills-MBP.domain>
2019-02-14 11:20:50 -0700

    finally fixed submodule issues

---
+++ client/src/App.js
@@ -0,0 +1,138 @@
+import React, { Component } from 'react';
+import {Jumbotron} from 'reactstrap';
+import Quiz from './components/Quiz'
+import Result from './components/Result'
+import quizQuestions from './utils/quizQuestions';
+import './App.css';
+
+class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state={
+      counter: 0, 
+      questionId: 1, 
+      question: '',
+      answerOptions: [],
+      answer: '',
+      answersCount: {
+        Nintendo: 0, 
+        Microsoft: 0, 
+        Sony: 0
+      },
+      result: ''
+    }
+
+    this.handleAnswerSelected = this.handleAnswerSelected.bind(this);
+  }
+
+  componentWillMount(){
+    const shuffledAnswerOptions = quizQuestions.map((question)=> this.shuffleArray(question.answers));
+
+    this.setState({
+      question: quizQuestions[0].question,
+      answerOptions: shuffledAnswerOptions[0]
+    });
+  }
+
+  shuffleArray(array) {
+   let currentIndex = array.length, temporaryValue, randomIndex;
+  
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+  
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+  
+      // And swap it with the current element.
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+  
+    return array;
+  };
+
+  setUserAnswer(answer) {
+    this.setState((state) => ({
+      answersCount: {
+        ...this.state.answersCount, 
+        [answer]: state.answersCount[answer] + 1
+       }, 
+       answer: answer
+    }));
+    console.log(answer);
+  }
+
+  setNextQuestion(){
+    const counter = this.state.counter + 1;
+    const questionId =  this.state.questionId +1;
+    this.setState({
+      counter: counter, 
+      questionId: questionId, 
+      question: quizQuestions[counter].question, 
+      answerOptions: quizQuestions[counter].answers,
+      answer: ''
+    });
+  }
+
+  handleAnswerSelected(event){
+    this.setUserAnswer(event.currentTarget.value);
+    if(this.state.questionId < quizQuestions.length){
+      setTimeout(() => this.setNextQuestion(), 300);
+    } else {
+      setTimeout(() => this.setResults(this.getResults()), 300)
+    }
+  }
+
+  getResults(){
+    const answersCount = this.state.answersCount;
+    const answersCountKeys = Object.keys(answersCount);
+    const answersCountValues = answersCountKeys.map((key) => answersCount[key]);
+    const maxAnswerCount = Math.max.apply(null, answersCountValues);
+    console.log(answersCount);
+    return answersCountKeys.filter((key) => answersCount[key] === maxAnswerCount)
+  }
+  
+  setResults (result) {
+    if(result.length === 1){
+      this.setState({result: result[0]});
+    }else {
+      this.setState({result: 'Undetermined'});
+    }
+  };
+
+  renderQuiz(){
+    return(
+      <Quiz 
+        answer={this.state.answer}
+        answerOptions={this.state.answerOptions}
+        counter={this.state.counter}s
+        questionId={this.state.questionId}
+        question={this.state.question}
+        questionTotal={quizQuestions.length}
+        onAnswerSelected={this.handleAnswerSelected}
+       />
+    );
+  }
+
+  renderResult() {
+    return (
+      <Result quizResult={this.state.result} />
+    );
+  }
+
+  render() {
+    return (
+      <div className="App">
+      <Jumbotron>
+      <h1>React Trivia Game!</h1>
+      </Jumbotron>
+      {this.state.result ? this.renderResult() : this.renderQuiz()}
+      </div>
+    );
+  }
+}
+
+export default App;
